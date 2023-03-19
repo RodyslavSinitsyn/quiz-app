a7a15d18bd941934e8f0883c26a411d885040c2d
Rodyslav Sinitsyn <r.sinitsyn@elsevier.com>
2023-03-19 11:53:21 +0200

    Initial commit

---
+++ src/main/java/org/rsinitsyn/quiz/QuizAppApplication.java
@@ -0,0 +1,16 @@
+package org.rsinitsyn.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class QuizAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizAppApplication.class, args);
+    }
+
+}
