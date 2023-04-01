3ed3673add04a0a6fbf4dfe18136aa199e683003
Rodyslav Sinitsyn <r.sinitsyn@elsevier.com>
2023-04-01 12:19:46 +0300

    Version 4.0
    - Music update
    - Answer without options gameplay
    - Question list improvements
    - Import process improvements
    - Have fun!

--- src/main/java/org/rsinitsyn/quiz/service/ImportService.java
+++ src/main/java/org/rsinitsyn/quiz/service/ImportService.java
@@ -12,11 +12,14 @@ import org.rsinitsyn.quiz.entity.AnswerEntity;
 import org.rsinitsyn.quiz.entity.QuestionEntity;
 import org.rsinitsyn.quiz.entity.QuestionType;
 import org.rsinitsyn.quiz.utils.QuizUtils;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class ImportService {
+
+    private static final String DEFAULT_DELIMITER = "|";
 
     private final QuestionService questionService;
 
@@ -42,19 +45,31 @@ public class ImportService {
         QuestionEntity entity = new QuestionEntity();
         entity.setText(tokens[0]);
 
-        entity.addAnswer(new AnswerEntity(tokens[1], true, 0));
-        entity.addAnswer(new AnswerEntity(tokens[2], false, 1));
-        entity.addAnswer(new AnswerEntity(tokens[3], false, 2));
-        entity.addAnswer(new AnswerEntity(tokens[4], false, 3));
+        for (int i = 0; i < 4; i++) {
+            Pair<String, Boolean> parsed = parseToken(tokens[i + 1]);
+            entity.addAnswer(new AnswerEntity(parsed.getFirst(), parsed.getSecond(), i));
+        }
+
+        long correctAnswersCount = entity.getAnswers().stream().filter(AnswerEntity::isCorrect).count();
+        if (correctAnswersCount == 0) {
+            throw new RuntimeException("Нет верных ответов для вопроса: " + tokens[0]);
+        }
+
         if (tokens.length > 5) {
             entity.setPhotoFilename(QuizUtils.generateFilename(tokens[5]));
             entity.setOriginalPhotoUrl(tokens[5]);
         }
         entity.setCreatedBy(QuizUtils.getLoggedUser());
         entity.setCreationDate(LocalDateTime.now());
-        entity.setType(QuestionType.TEXT);
+        entity.setType(correctAnswersCount > 1 ? QuestionType.MULTI : QuestionType.TEXT);
         entity.setCategory(questionService.getOrCreateDefaultCategory());
 
         return entity;
+    }
+
+    private Pair<String, Boolean> parseToken(String tokenValue) {
+        boolean correct = tokenValue.startsWith("_");
+        String val = correct ? tokenValue.substring(1) : tokenValue;
+        return Pair.of(val, correct);
     }
 }
