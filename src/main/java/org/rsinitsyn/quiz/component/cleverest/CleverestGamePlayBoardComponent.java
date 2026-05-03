1062cf1b8fc14e258bccec3ded6dedc6850d3791
“rsinitsyn” <radik200058@gmail.com>
2026-05-03 15:52:29 +0200

    Add more meme sounds

--- src/main/java/org/rsinitsyn/quiz/component/cleverest/CleverestGamePlayBoardComponent.java
+++ src/main/java/org/rsinitsyn/quiz/component/cleverest/CleverestGamePlayBoardComponent.java
@@ -23,6 +23,7 @@ import org.rsinitsyn.quiz.model.QuestionModel;
 import org.rsinitsyn.quiz.model.answer.AnswerBet;
 import org.rsinitsyn.quiz.model.cleverest.*;
 import org.rsinitsyn.quiz.model.sound.GameSound;
+import org.rsinitsyn.quiz.model.sound.GameSounds;
 import org.rsinitsyn.quiz.service.CleverestBroadcaster;
 import org.rsinitsyn.quiz.service.CleverestBroadcaster.*;
 
@@ -476,7 +477,7 @@ public class CleverestGamePlayBoardComponent extends VerticalLayout {
 
         topContainer.add(userProfileWithScore(userState.snapshot(), MOBILE_LARGE_FONT));
         topContainer.add(horizontalLayoutCenter(
-                soundButton(() -> broadcaster.sendPlaySoundEvent(gameId, GameSound.next())), // TODO: Better meme handling and chosing
+                soundButton(() -> broadcaster.sendPlaySoundEvent(gameId, GameSounds.next())), // TODO: Better meme handling and chosing
                 openChatButton(messageText -> broadcaster.sendUserTextedEvent(gameId, getLoggedUser(), messageText)),
                 reactionButton(Emoji.HEART.value, (emoji) -> broadcaster.sendLiveReactionEvent(gameId, getLoggedUser(), emoji))));
         topContainer.add(new Hr());
---
+++ src/main/java/org/rsinitsyn/quiz/model/sound/GameSounds.java
@@ -0,0 +1,44 @@
+package org.rsinitsyn.quiz.model.sound;
+
+import com.google.common.collect.Iterables;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class GameSounds {
+
+    private static final List<GameSound> ALL = load();
+
+    private static final Iterator<GameSound> CYCLE =
+            Iterables.cycle(ALL).iterator();
+
+    private static List<GameSound> load() {
+        try (final var stream = Files.list(Path.of("src/main/resources/audio/static/sounds"))) {
+            return stream
+                    .filter(Files::isRegularFile)
+                    .map(path -> new GameSound(
+                            path.getFileName().toString(),
+                            SoundCategory.FUNNY
+                    ))
+                    .toList();
+        } catch (IOException e) {
+            throw new IllegalStateException("Failed to load sounds", e);
+        }
+    }
+
+    public static GameSound next() {
+        return CYCLE.next();
+    }
+
+    public static GameSound random(SoundCategory category) {
+        final var sounds = ALL.stream()
+                .filter(sound -> sound.category() == category)
+                .toList();
+
+        return sounds.get(ThreadLocalRandom.current().nextInt(sounds.size()));
+    }
+}
